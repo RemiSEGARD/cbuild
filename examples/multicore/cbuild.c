bf792ca290ca101d0b23100008f796e05dc9f2bd
Remi SEGARD <remi.segard@epita.fr>
2024-02-10 15:04:17 +0100

    [cbuild] multicore build

---
+++ examples/multicore/cbuild.c
@@ -0,0 +1,53 @@
+#include <string.h>
+#define CBUILD_IMPLEMENTATION
+#include "../../cbuild.h"
+
+int main(int argc, char *argv[])
+{
+    CBUILD_REBUILD_YOURSELF(argc, argv);
+
+    static cbuild_target file1_o = CBUILD_TARGET("file1.o",
+            CBUILD_MAKE_FILE_SOURCE("file1.c")
+            );
+    static cbuild_target file2_o = CBUILD_TARGET("file2.o",
+            CBUILD_MAKE_FILE_SOURCE("file2.c")
+            );
+    static cbuild_target file3_o = CBUILD_TARGET("file3.o",
+            CBUILD_MAKE_FILE_SOURCE("file3.c")
+            );
+    static cbuild_target file4_o = CBUILD_TARGET("file4.o",
+            CBUILD_MAKE_FILE_SOURCE("file4.c")
+            );
+    static cbuild_target file5_o = CBUILD_TARGET("file5.o",
+            CBUILD_MAKE_FILE_SOURCE("file5.c")
+            );
+    static cbuild_target main_o = CBUILD_TARGET("main.o",
+            CBUILD_MAKE_FILE_SOURCE("main.c")
+            );
+    static cbuild_target _main = CBUILD_TARGET("main",
+            CBUILD_MAKE_TARGET_SOURCE(&file1_o),
+            CBUILD_MAKE_TARGET_SOURCE(&file2_o),
+            CBUILD_MAKE_TARGET_SOURCE(&file3_o),
+            CBUILD_MAKE_TARGET_SOURCE(&file4_o),
+            CBUILD_MAKE_TARGET_SOURCE(&file5_o),
+            CBUILD_MAKE_TARGET_SOURCE(&main_o)
+            );
+
+    if (argc > 1 && strcmp(argv[1], "--clean") == 0)
+    {
+        cbuild_clean_target(&_main);
+        return 0;
+    }
+
+    cbuild_str_vector_add_str(&_main.command, "c++");
+    for (size_t i = 0; _main.sources[i].source_type; i++)
+        cbuild_str_vector_add_strs(&_main.sources[i].source.target->command,
+                                   "c++", "-c");
+
+    if (cbuild_multiprocess_build_target(&_main, NULL, 0, 4))
+    {
+        cbuild_log(CBUILD_ERROR, "Could not build target `main'");
+        return 1;
+    }
+    return 0;
+}
---
+++ examples/multicore/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int f1();
+int f2();
+int f3();
+int f4();
+int f5();
+
+int main()
+{
+    printf("%d %d %d %d %d\n", f1(), f2(), f3(), f4(), f5());
+}
